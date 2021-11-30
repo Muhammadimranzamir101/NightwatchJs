270a654c83a0041710cdaeb91dd6014b09de571b
Muhammad Imran <muhammad.imran@backcountry.com>
2021-11-30 16:53:25 +0500

    Fixing Driver Issue

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -29,7 +29,7 @@ module.exports = {
             webdriver: {
                 start_process: true,
                 port: 9515,
-                server_path : "node_modules/.bin/chromedriver"
+                server_path : "node_modules/chromedriver/bin/chromedriver"
                 //server_path: require('chromedriver').path,
             }
         },
